2c3e0047528eeda6c98392d4de93608ec8a32d6d
bit <dltjrwls221@gmail.com>
2020-08-21 08:46:55 +0900

    실시간 채팅 ChatController.java 에서 return "/chatting"; 으로 바꾸고 chatting.jsp에서 groupbattle.duckdns.org:3000 으로 된 2개를 채팅 서버 주소로 변경

--- src/main/java/com/test02/Controller/MailController.java
+++ src/main/java/com/test02/Controller/MailController.java
@@ -26,12 +26,12 @@ public class MailController{
     @SneakyThrows
     @RequestMapping(value = "/mailSender")
     public void mailSender(HttpServletRequest request, HttpServletResponse response) { // 네이버일 경우 smtp.naver.com 을 입력합니다. // Google일 경우 smtp.gmail.com 을 입력합니다.
-        String host = "smtp.modumj.duckdns.org";
+        String host = "smtp.modumjeon.duckdns.org";
         final String username = "public"; //네이버 아이디를 입력해주세요. @nave.com은 입력하지 마시구요.
         final String password = "zusdhkd"; //네이버 이메일 비밀번호를 입력해주세요.
         int port = 465; //포트번호
         // 메일 내용
-        String recipient = "modum@modumj.duckdns.org"; //받는 사람의 메일주소를 입력해주세요.
+        String recipient = "modum@modumjeon.duckdns.org"; //받는 사람의 메일주소를 입력해주세요.
         String subject = request.getParameter("title"); //"hello 메일테스트"; //메일 제목 입력해주세요.
         String body = request.getParameter("content"); //username + "님으로 부터 메일을 받았습니다."; //메일 내용 입력해주세요.
         Properties props = System.getProperties(); // 정보를 담기 위한 객체 생성 // SMTP 서버 정보 설정
@@ -53,7 +53,7 @@ public class MailController{
         try {
             session.setDebug(true); //for debug
             MimeMessage mimeMessage = new MimeMessage(session); //MimeMessage 생성
-            mimeMessage.setFrom(new InternetAddress("public@modumj.duckdns.org")); //발신자 셋팅 , 보내는 사람의 이메일주소를 한번 더 입력합니다. 이때는 이메일 풀 주소를 다 작성해주세요.
+            mimeMessage.setFrom(new InternetAddress("public@modumjeon.duckdns.org")); //발신자 셋팅 , 보내는 사람의 이메일주소를 한번 더 입력합니다. 이때는 이메일 풀 주소를 다 작성해주세요.
             mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient)); //수신자셋팅 //.TO 외에 .CC(참조) .BCC(숨은참조) 도 있음
             mimeMessage.setSubject(MimeUtility.encodeText(subject, "UTF-8", "B")); //제목셋팅
             mimeMessage.setText(body, "UTF-8"); //내용셋팅0
